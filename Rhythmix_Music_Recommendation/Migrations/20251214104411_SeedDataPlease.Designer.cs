da0c0345f73c3df5307949a250af2ba9b7b99cf8
Jonathan Quek <2401302A@student.tp.edu.sg>
2025-12-14 18:52:30 +0800

    Seeding Data for Staff and User

--- Rhythmix_Music_Recommendation/Migrations/20251214104411_SeedDataPlease.Designer.cs
+++ Rhythmix_Music_Recommendation/Migrations/20251214104411_SeedDataPlease.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Rhythmix_Music_Recommendation.Data;
 
@@ -11,9 +12,11 @@ using Rhythmix_Music_Recommendation.Data;
 namespace Rhythmix_Music_Recommendation.Migrations
 {
     [DbContext(typeof(Rhythmix_Music_RecommendationContext))]
-    partial class Rhythmix_Music_RecommendationContextModelSnapshot : ModelSnapshot
+    [Migration("20251214104411_SeedDataPlease")]
+    partial class SeedDataPlease
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
